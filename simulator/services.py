aa4730d3f57449e2d69bf79f1ed119c4ff11c68b
Alex P <alexpod1000@gmail.com>
2018-12-06 15:19:14 +0100

    Minor code style correction.

--- simulator/services.py
+++ simulator/services.py
@@ -3,21 +3,21 @@ import math
 class CoordinateProvider:
 
     def __init__(self, initialLocation, spacing):
-        self.initialLocation = { "latitude": initialLocation[0], "longitude": initialLocation[1] };
-        self.spacing = spacing;
-        self.coef = self.spacing * 0.0000089; # meters in degrees
+        self.initialLocation = { "latitude": initialLocation[0], "longitude": initialLocation[1] }
+        self.spacing = spacing
+        self.coef = self.spacing * 0.0000089 # meters in degrees
 
     def element_at(self, i, j):
         """
         i: vertical quadrant index
         j: horizontal quadrant index
         """
-        cosSum = 0;
+        cos_sum = 0
         for k in range(j):
-            cosSum += 1 / math.cos((self.initialLocation["latitude"] + k * self.coef) * 0.018); # 0.018 is pi/180
-        new_lat = self.initialLocation["latitude"]  + i * self.coef;
-        new_lon = self.initialLocation["longitude"]  + self.coef * cosSum;
-        return [new_lat, new_lon];
+            cos_sum += 1 / math.cos((self.initialLocation["latitude"] + k * self.coef) * 0.018) # 0.018 is pi/180
+        new_lat = self.initialLocation["latitude"]  + i * self.coef
+        new_lon = self.initialLocation["longitude"]  + self.coef * cos_sum
+        return [new_lat, new_lon]
 
     def find_interval(self, lat, lon):
         i = 0
