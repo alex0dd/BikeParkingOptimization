b4c3359cb34e50bf80acc02bcee3a81ed87f8751
Alex P <alexpod1000@gmail.com>
2018-12-13 12:09:59 +0100

    Added coordinate approximation in client aswell.

--- client/js/services/CoordinateProvider.js
+++ client/js/services/CoordinateProvider.js
@@ -3,17 +3,19 @@ export default class CoordinateProvider {
         this.initialLocation = { latitude: initialLocation[0], longitude: initialLocation[1] };
         this.spacing = spacing;
         this.coef = this.spacing * 0.0000089; // meters in degrees
+        this.cosSumApprox = 1/Math.cos((this.initialLocation.latitude+this.coef)*0.018);
     }
     elementAt(i, j) {
         /*
             i = vertical quadrant index
             j = horizontal quadrant index
         */
-        var cosSum = 0;
-        for (var k = 0; k < j; k++)
-            cosSum += 1 / Math.cos((this.initialLocation.latitude + k * this.coef) * 0.018); // 0.018 is pi/180
+	/*
+    	var cosSum = 0;
+	for (var k = 0; k < j; k++)
+            cosSum += 1 / Math.cos((this.initialLocation.latitude + k * this.coef) * 0.018); // 0.018 is pi/180*/
         var new_lat = this.initialLocation.latitude + i * this.coef;
-        var new_lon = this.initialLocation.longitude + this.coef * cosSum;
+        var new_lon = this.initialLocation.longitude + this.coef * this.cosSumApprox*j;
         return [new_lat, new_lon];
     }
 };
